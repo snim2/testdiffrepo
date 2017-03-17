c5e4870f1163a109408086aa7c25f97951442da1
Sarah Mount <sarah.mount@kcl.ac.uk>
2017-03-17 12:43:57 +0000

    Differ diffs.

--- differ.py
+++ differ.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python2.7
 
+import difflib
 import sys
 
 if __name__ == '__main__':
-    print sys.argv
+    print 'XYZ DIFFER.'
+    if sys.argv[4] != sys.argv[7]:
+        print 'File permissions differ.'
+    file1 = ''
+    file2 = ''
+    with open(sys.argv[2], 'r') as fd:
+        file1 = fd.readlines()
+    with open(sys.argv[5], 'r') as fd:
+        file2 = fd.readlines()
+    diff = difflib.unified_diff(file1, file2)
+    diff_string = ''.join(diff)
+    if diff_string:
+        print diff_string
+    else:
+        print 'Files are identical.'
